152aff457644f57587fee07b30b4fa8489b248fe
chanwoo ahn <cwahn@yonsei.ac.kr>
2020-01-19 12:34:45 +0000

    Stablize for deployment.
    First deployment version on homeserver.
    Venv has been remade with python 3.7.

--- homeserver/bin/django-admin.py
+++ homeserver/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/chanwooahn/Google Drive/Projects/Web/home-page-project-2/homeserver/bin/python
+#!/home/cwahn/home-server-2/homeserver/bin/python3.7
 from django.core import management
 
 if __name__ == "__main__":
