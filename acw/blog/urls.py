1ad8c5ea4f655d49596ced9fc8c505513845477c
chanwoo ahn <cwahn@yonsei.ac.kr>
2020-02-01 13:26:07 +0000

    Change LaTeX renderer setting
     Commit for migration to envy 17 acwserver.

--- acw/blog/urls.py
+++ acw/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('model_test/', views.model_test, name='model_test'),
-    path('', views.index),
+    path('', views.index, name='index'),
     path('article_list/<slug:tag>', views.article_list, name='article_list'),
     path('article/<int:pk>', views.article, name='article'),
     path('work_list/<slug:tag>', views.comming_soon, name='work_list'),
